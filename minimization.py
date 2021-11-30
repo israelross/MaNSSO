e221e242e64cffb37ce1beee45505e005197d998
israel rosset <israelrosset@israels-MacBook-Air.local>
2021-11-30 10:23:24 +0200

    Added documentation

--- minimization.py
+++ minimization.py
@@ -4,6 +4,14 @@ import numpy.linalg as la
 
 class SmoothingManifoldManimizer:
     def __init__(self, manifold, f, g, transformation=None):
+        """
+        Solves the problem \argmin_{x\in M} f(x) + g(Ax) using Algorithm 2.
+        where A=transformation, M=manifold. f is the smooth part and g the non-smooth convex part.
+        :param manifold: of class Manifold
+        :param f: of class SmoothFunction
+        :param g: of class NonSmoothConvexFunction
+        :param transformation: np.array
+        """
         self._A = transformation
         self._M = manifold
         self._f = f
@@ -45,17 +53,32 @@ class SmoothingManifoldManimizer:
                 return None
         return point
 
-    def optimize(self, method='constant', max_iter=1000, mu_oracle='std', ret_iter_list=False, init_point=None, ret_counter=False):
-        if method == 'constant':
+    def optimize(self, stepsize_strategy='backtrack', max_iter=1000, mu_oracle='std',
+                 ret_iter_list=False, init_point=None, ret_counter=False):
+        """
+        :param stepsize_strategy: 'diminishing' and 'backtrack' according to the paper
+        :param max_iter:          number. Maximum of iterations to perform
+        :param mu_oracle:         'std' for k^{-1/3}. A function k \to \mu_k otherwise.
+        :param ret_iter_list:     Boolean. Whether to return the value of each iteration or not.
+        :param init_point:        Either a point on the manifold or None for random point.
+        :param ret_counter:       Boolean. Can be True  if the backtracking strategy is chosen.
+                                  returns the number of retraction steps performed until each iteration.
+        :return:                  if ret_iter_list is False - returns a point on the manifold
+                                  if ret_iter_list is True  - returns a tuple of two lists -
+                                        the first is the calculated point on each iteration. The second is empty if
+                                        ret_iter_list is False. Otherwise, it is a list of the numbers of retractions
+                                        performed from the beginning of the algorithm until the end of each iteration.
+        """
+        if stepsize_strategy == 'diminishing':
             next_point = self.lipschitz_next_point
-        elif method == 'backtrack':
+        elif stepsize_strategy == 'backtrack':
             self._curr_bt = self._start
             next_point = self.backtrack_next_point
         else:
             raise Exception("Bad method")
 
         if mu_oracle == 'std':
-            mu_oracle = lambda n: 0.01 * np.power(n, -1/3)
+            mu_oracle = lambda n: np.power(n, -1/3)
 
         if ret_iter_list:
             iter_list = []
@@ -93,12 +116,28 @@ class SmoothingManifoldManimizer:
 
 class SubgradientManifoldMinimizer:
     def __init__(self, manifold, f, g, transformation=None):
+        """
+        Solves the problem \argmin_{x\in M} f(x) + g(Ax) using Riemmanian Subgradient Method.
+        where A=transformation, M=manifold. f is the smooth part and g the non-smooth convex part.
+        :param manifold: of class Manifold
+        :param f: of class SmoothFunction
+        :param g: of class NonSmoothConvexFunction
+        :param transformation: np.array
+        """
         self._A = transformation
         self._M = manifold
         self._f = f
         self._g = g
 
     def optimize(self, max_iter=1000, ret_iter_list=False, init_point=None):
+        """
+
+        :param max_iter:          number. Maximum of iterations to perform
+        :param ret_iter_list:     Boolean. Whether to return the value of each iteration or not.
+        :param init_point:        Either a point on the manifold or None for random point.
+        :return:                  if ret_iter_list is False - returns a point on the manifold
+                                  if ret_iter_list is True  - returns a list of the calculated point on each iteration.
+        """
         if ret_iter_list:
             iter_list = []
 
@@ -126,36 +165,38 @@ class SubgradientManifoldMinimizer:
 
         return point
 
-class MADMMinimizer:
-    def __init__(self, manifold, f, g, rho, transformation=None):
-        self._A = transformation
-        self._M = manifold
-        self._f = f
-        self._g = g
-        self._rho = rho
 
-    def optimize(self, max_iter=1000, ret_iter_list=False, init_point=None):
-        if ret_iter_list:
-            iter_list = []
-
-        if init_point is None:
-            point = self._M.random_point()
-        else:
-            point = init_point
-
-        x = point
-        z = np.copy(x)
-        u = np.zeros(x.shape)
-        for i in range(1, max_iter):
-            if ret_iter_list:
-                iter_list.append(x)
-            if self._A is not None:
-                raise Exception("Error")
-            else:
-                v = -self._M.project_vector_to_tangent(x, self._f.grad(x) + self._rho * (x - z + u))
-            x = self._M.retract(x, v)
-            z = self._g.prox(x + u, 1/self._rho)
-            u += x - z
-
-        if ret_iter_list:
-            return np.array(iter_list)
+#
+# class MADMMinimizer:
+#     def __init__(self, manifold, f, g, rho, transformation=None):
+#         self._A = transformation
+#         self._M = manifold
+#         self._f = f
+#         self._g = g
+#         self._rho = rho
+#
+#     def optimize(self, max_iter=1000, ret_iter_list=False, init_point=None):
+#         if ret_iter_list:
+#             iter_list = []
+#
+#         if init_point is None:
+#             point = self._M.random_point()
+#         else:
+#             point = init_point
+#
+#         x = point
+#         z = np.copy(x)
+#         u = np.zeros(x.shape)
+#         for i in range(1, max_iter):
+#             if ret_iter_list:
+#                 iter_list.append(x)
+#             if self._A is not None:
+#                 raise Exception("Error")
+#             else:
+#                 v = -self._M.project_vector_to_tangent(x, self._f.grad(x) + self._rho * (x - z + u))
+#             x = self._M.retract(x, v)
+#             z = self._g.prox(x + u, 1/self._rho)
+#             u += x - z
+#
+#         if ret_iter_list:
+#             return np.array(iter_list)
--- run.py
+++ run.py
@@ -23,7 +23,6 @@ cov = np.array([[1.000,  0.954,  0.364,  0.342, -0.129,  0.313,  0.496,  0.424, 
                 [0.019, -0.036,  0.220,  0.169, -0.145,  0.024, -0.232, -0.357, -0.127, -0.368,  0.029,  1.000,  0.184],
                 [0.134,  0.144,  0.126,  0.015, -0.208, -0.329, -0.424, -0.202, -0.076, -0.291,  0.007,  0.184,  1.000]]
                )
-p = 6
 np.set_printoptions(precision=2)
 zou_res = np.array([[-0.42,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ],
        [-0.42,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ],
@@ -42,39 +41,33 @@ zou_res = np.array([[-0.42,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ],
 
 class SPCAComparingMinimizers:
     def __init__(self, cov, p, lam):
+        print(p)
         self._d = cov.shape[0]
         self._M = StiefelManifold(self._d, p)
         self._f = L2PCA(cov, p, 'cov')
         self._g = L1Norm(cov, p, lam)
         self._sminimizer = SmoothingManifoldManimizer(self._M, self._f, self._g)
         self._gminimizer = SubgradientManifoldMinimizer(self._M, self._f, self._g)
-        self._aminimizer = MADMMinimizer(self._M, self._f, self._g, 1)
         self._sminimizer.set_backtrack_params(factor=0.5, threshold=0.5, start=1)
 
     def minimize(self, iters=1000, plot=True):
-        # self.a_res = self.aminimize(ret_iter_list=True, max_iter=iters)
         self.s1_res, self.s1_cnt = self.sminimize(ret_iter_list=True, max_iter=iters)
         self.s2_res, self.s2_cnt = self.sminimize(ret_iter_list=True, max_iter=iters, mu_oracle=lambda i: 0.1*np.power(i, -1 / 2))
         self.g_res = self.gminimize(ret_iter_list=True, max_iter=iters)
         self.s1_fun = np.array([minimizer.perform(i) for i in self.s1_res])
         self.s2_fun = np.array([minimizer.perform(i) for i in self.s2_res])
         self.g_fun = np.array([minimizer.perform(i) for i in self.g_res])
-        # self.a_fun = np.array([minimizer.perform(i) for i in self.a_res])
         if plot:
-            plt.plot(self.s1_cnt, self.s1_fun, label=r'Algorithm 2, $\mu_k=\frac{1}{k^{1/3}}$')
-            plt.plot(self.s2_cnt, self.s2_fun, label=r'Algorithm 2, $\mu_k=\frac{0.1}{\sqrt{k}}$')
-            plt.plot(range(self.g_fun), self.g_fun, label='RSG')
-            # plt.plot(self.a_fun)
+            plt.plot(np.cumsum(self.s1_cnt), self.s1_fun, label=r'Algorithm 2, $\mu_k=\frac{1}{k^{1/3}}$')
+            plt.plot(np.cumsum(self.s2_cnt), self.s2_fun, label=r'Algorithm 2, $\mu_k=\frac{0.1}{\sqrt{k}}$')
+            plt.plot(range(len(self.g_fun)), self.g_fun, label='RSG')
 
     def sminimize(self, mu_oracle=lambda i: 1*np.power(i, -1 / 3), max_iter=1000, ret_iter_list=False):
         return self._sminimizer.optimize(max_iter=max_iter, mu_oracle=mu_oracle,
-                                        method='backtrack', ret_iter_list=ret_iter_list, ret_counter=True)
+                                        stepsize_strategy='backtrack', ret_iter_list=ret_iter_list, ret_counter=True)
 
     def gminimize(self, max_iter=1000, ret_iter_list=False):
         return self._gminimizer.optimize(max_iter=max_iter, ret_iter_list=ret_iter_list)
-
-    def aminimize(self, max_iter=1000, ret_iter_list=False):
-        return self._aminimizer.optimize(max_iter=max_iter, ret_iter_list=ret_iter_list)
 
     def perform(self, x):
         return self._f.perform(x) + self._g.perform(x)
@@ -86,20 +79,9 @@ f = open('isolet1X.pickle', 'rb')
 data = pickle.load(f)
 cov = np.corrcoef(data.T)
 minimizer = SPCAComparingMinimizers(cov, 15, 0.5)
-minimizer.minimize(iters=1000, plot=False)
-#
-# lam=0.5
-# d = cov.shape[0]
-# M = StiefelManifold(d, p)
-# f = L2PCA(cov, p, 'cov')
-# g = L1Norm(cov, p, lam)
-# minimizer = MADMMinimizer(M, f, g, 10)
-# a_res = minimizer.optimize(max_iter=100, ret_iter_list=True)
-# a_fun = np.array([f.perform(i) + g.perform(i) for i in a_res])
-# plt.plot(a_fun)
+minimizer.minimize(iters=1000, plot=True)
 
-
-# plt.xlabel("#iteration")
-# plt.ylabel("cost")
-# plt.legend()
-# plt.show()
+plt.xlabel("#retractions")
+plt.ylabel("cost")
+plt.legend()
+plt.show()
